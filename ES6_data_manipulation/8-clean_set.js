810342703764d5639bfa4f7cc35b93a5359ee47d
alexpr42 <8988@holbertonstudents.com>
2024-07-23 00:12:40 -0400

    8-clean_set.js

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,11 @@
+// 8-clean_set.js
 function cleanSet(set, startString = '') {
   if (typeof startString !== 'string') {
     throw new TypeError('startString must be a string');
   }
 
   return [...set]
-    .filter((value) => value.startsWith(startString))
+    .filter((value) => typeof value === 'string' && value.startsWith(startString))
     .map((value) => value.slice(startString.length))
     .join('-');
 }
