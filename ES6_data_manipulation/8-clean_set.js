e05476cf8ee46fb0a74ae82f984cf7c4a8e14e18
alexpr42 <8988@holbertonstudents.com>
2024-07-23 00:11:21 -0400

    8-clean_set.js

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,8 @@
-function cleanSet(set, startString) {
+function cleanSet(set, startString = '') {
+  if (typeof startString !== 'string') {
+    throw new TypeError('startString must be a string');
+  }
+
   return [...set]
     .filter((value) => value.startsWith(startString))
     .map((value) => value.slice(startString.length))
