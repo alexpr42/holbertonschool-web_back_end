ddbb024a799cdfcb22d49900379109ed373a373c
alexpr42 <8988@holbertonstudents.com>
2024-07-25 19:10:18 -0400

    9-try.js

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -1,14 +1,14 @@
 export default function guardrail(mathFunction) {
   const queue = [];
-  
+
   try {
     const result = mathFunction();
     queue.push(result);
   } catch (error) {
-    queue.push(error.message);
+    queue.push(error.message); // Use error.message to get just the error message
   } finally {
     queue.push('Guardrail was processed');
   }
-  
+
   return queue;
 }
