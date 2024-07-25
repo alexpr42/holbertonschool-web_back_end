b6825c5f4234dadf90f5a70bf27799cc1a2bb0bc
alexpr42 <8988@holbertonstudents.com>
2024-07-25 16:31:09 -0400

    0-promise.js

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,8 @@
+function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // You can add some async logic here if needed
+    resolve('Response from API');
+  });
+}
+
+export default getResponseFromAPI;
