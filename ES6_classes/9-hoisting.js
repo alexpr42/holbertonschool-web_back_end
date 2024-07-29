0aed55cf581b9db6bbef04b4dc3bdf657a4f76c7
alexpr42 <8988@holbertonstudents.com>
2024-07-29 00:32:59 -0400

    9-hoisting.js

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -33,11 +33,11 @@ export class StudentHolberton {
   }
 }
 
-// Create instances of HolbertonClass
+// Instances of HolbertonClass
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
-// Create instances of StudentHolberton
+// Instances of StudentHolberton
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
 const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
