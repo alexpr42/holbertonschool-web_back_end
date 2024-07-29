9a1ad2579b8d94fb5526845ac6669ba11e4ba28e
alexpr42 <8988@holbertonstudents.com>
2024-07-29 00:13:45 -0400

    2-hbtn_course.js

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -4,24 +4,24 @@ export default class HolbertonCourse {
     this._length = length;
     this._students = students;
 
-    // contructor validator
+    // Validación en el constructor
     if (typeof this._name !== 'string') {
       throw new TypeError('Name must be a string');
     }
     if (typeof this._length !== 'number') {
       throw new TypeError('Length must be a number');
     }
-    if (!Array.isArray(this._students) || !this._students.every(student => typeof student === 'string')) {
+    if (!Array.isArray(this._students) || !this._students.every((student) => typeof student === 'string')) {
       throw new TypeError('Students must be an array of strings');
     }
   }
 
-  // Getter for name
+  // Getter para name
   get name() {
     return this._name;
   }
 
-  // Setter for name
+  // Setter para name
   set name(value) {
     if (typeof value !== 'string') {
       throw new TypeError('Name must be a string');
@@ -29,12 +29,12 @@ export default class HolbertonCourse {
     this._name = value;
   }
 
-  // Getter for length
+  // Getter para length
   get length() {
     return this._length;
   }
 
-  // Setter for length
+  // Setter para length
   set length(value) {
     if (typeof value !== 'number') {
       throw new TypeError('Length must be a number');
@@ -42,14 +42,14 @@ export default class HolbertonCourse {
     this._length = value;
   }
 
-  // Getter for students
+  // Getter para students
   get students() {
     return this._students;
   }
 
-  // Setter for students
+  // Setter para students
   set students(value) {
-    if (!Array.isArray(value) || !value.every(student => typeof student === 'string')) {
+    if (!Array.isArray(value) || !value.every((student) => typeof student === 'string')) {
       throw new TypeError('Students must be an array of strings');
     }
     this._students = value;
