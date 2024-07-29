089e3ed358cfeb9d90f1438091d8fe72e20e8b7d
alexpr42 <8988@holbertonstudents.com>
2024-07-29 00:17:58 -0400

    4-pricing.js

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,6 +1,32 @@
-import Pricing from './4-pricing.js';
 import Currency from './3-currency.js';
 
-const p = new Pricing(100, new Currency("EUR", "Euro"));
-console.log(p);
-console.log(p.displayFullPrice());
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    this._amount = value;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
