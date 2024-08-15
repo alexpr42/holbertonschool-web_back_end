b1cf214362bcecc38f59d51e696b38da89686f99
alexpr42 <8988@holbertonstudents.com>
2024-08-14 20:14:31 -0400

    3-to_str.py

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,4 +1,8 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
-def to_str(n: float) ->str:
+"""fuction that show float as string"""
+
+
+def to_str(n: float) -> str:
+    """the return that show the str"""
     return str(n)
