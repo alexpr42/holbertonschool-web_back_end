4fe72114d06e31731b4d33012d1fdafc7d040392
alexpr42 <8988@holbertonstudents.com>
2024-08-15 22:40:15 -0400

    1-concurrent_coroutines.py

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
 import asyncio
 from typing import List
-from 0-basic_async_syntax import wait_random
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """Wait for a random delay between 0 and max_delay
+    (inclusive) seconds."""
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Execute wait_random n times and return list of delays
     in ascending order."""
-
     # Create a list of coroutine tasks
-    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    tasks = [wait_random(max_delay) for _ in range(n)]
 
     # Gather all results as they complete
     delays = []
