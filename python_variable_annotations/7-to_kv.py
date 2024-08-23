cae180a8597b11eeec37c2d0f60a940e1c3f7773
alexpr42 <8988@holbertonstudents.com>
2024-08-23 10:30:44 -0400

    7-to_kv.py

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -7,9 +7,6 @@
 from typing import Union, Tuple
 
 
-""" return the string and the float at square"""
-
-
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    # return of the str and float at square
+    """ return the string and the float at square"""
     return (k, float(v ** 2))
