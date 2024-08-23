b62d5f499e677dc71779f3fffc33405c86d050b9
alexpr42 <8988@holbertonstudents.com>
2024-08-23 10:54:08 -0400

    1-concurrent

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,20 +1,16 @@
 #!/usr/bin/env python3
+"""
+Asynchronously wait for a random delay between 0 and
+max_delay (inclusive) seconds.
+"""
+
+
 import asyncio
 from typing import List
 import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    """
-    Asynchronously wait for a random delay between 0 and
-    max_delay (inclusive) seconds.
-
-    Args:
-     max_delay (int): The maximum delay in seconds. Defaults to 10.
-
-    Returns:
-    float: The delay time in seconds.
-    """
     # Generate a random float between 0 and max_delay
     delay = random.uniform(0, max_delay)
     # Asynchronously wait for the generated delay
