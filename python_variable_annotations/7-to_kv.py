e1726277e153a5ee72ff863fb47b36a690fc238b
alexpr42 <8988@holbertonstudents.com>
2024-08-23 10:26:41 -0400

    7-to_kv.py

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
-from typing import Union, Tuple
+
 
 """ function that thake a string and float arguments"""
+
+
+from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
