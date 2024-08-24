0e8ee6c377ae666f13a514515a4a7262bc86e46f
alexpr42 <8988@holbertonstudents.com>
2024-08-23 22:48:09 -0400

    1-async_comprehension.py

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -3,7 +3,9 @@
 
 
 import asyncio
-from 0-async_generator import async_generator
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
 
 async def async_comprehension():
     return [i async for i in async_generator()]
