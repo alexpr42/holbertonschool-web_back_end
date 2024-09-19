b3a4231b9c81ac4401370df4571ffb5eebb37bfc
alexpr42 <8988@holbertonstudents.com>
2024-09-19 19:58:14 -0400

    7-http_express.js

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -2,23 +2,30 @@ const express = require('express');
 const countStudents = require('./3-read_file_async');
 
 const app = express();
-const port = 1245;
-const path = process.argv[2];
-
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
 app.get('/students', async (req, res) => {
-  const msg = 'This is the list of our students\n';
+  const dbpath = process.argv[2];
+  let o = 'This is the list of our students\n';
+  if (!dbpath) {
+    o += 'Cannot load the database';
+    res.status(500).send(o);
+    return;
+  }
   try {
-    const students = await countStudents(path);
-    res.send(`${msg}${students.join('\n')}`);
-  } catch (err) {
-    res.send(`${msg}${err.message}`);
+    const studentsData = await countStudents(dbpath);
+    o += studentsData;
+    res.send(o);
+  } catch (error) {
+    o += 'Cannot load the database';
+    res.send(o);
   }
 });
 
-app.listen(port);
+app.listen(1245, () => {
+  console.log('Express server is listening on port 1245');
+});
 
 module.exports = app;
