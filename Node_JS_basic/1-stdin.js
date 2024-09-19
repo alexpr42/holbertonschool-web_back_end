ebfd183e81f16d00f9a5b91703ed3212c644c19c
alexpr42 <8988@holbertonstudents.com>
2024-09-19 19:40:38 -0400

    1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,19 +1,14 @@
-const readline = require('readline');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+process.stdin.setEncoding('utf8');
 
-console.log('Welcome to Holberton School, what is your name?');
-
-rl.question('', (name) => {
-  if (name) {
-    console.log(`Your name is: ${name}`);
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name}`);
   }
-  rl.close();
 });
 
-rl.on('close', () => {
-  console.log('This important software is now closing');
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
